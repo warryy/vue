e3c85a561f004aee8136ae109e1ac7a16697f575
warryy <warryy555@126.com>
2020-05-21 02:16:13 +0800

    feat: route-view history模式最简功能

--- myvue/vue-cli/demo/src/kvue-router/yrouter-view.js
+++ myvue/vue-cli/demo/src/kvue-router/yrouter-view.js
@@ -1,6 +1,17 @@
 
 export default {
     render(h) {
-        return h(null);
+        let component = null;
+        // 获取到 路由配置表
+        let routes = this.$router.$options.routes;
+        
+        routes.forEach(route => {
+            // 找到当前路径对应的路由, 将路由的 vue 页面组件配置传入到 h 函数中
+            if (route.path === this.$router.current) {
+                component = route.component;
+            }
+        });
+
+        return h(component);
     }
 }
--- myvue/vue-cli/demo/src/kvue-router/yvue-router.js
+++ myvue/vue-cli/demo/src/kvue-router/yvue-router.js
@@ -4,8 +4,22 @@ import Link from '@/kvue-router/yrouter-link';
 let Vue;
 
 class router {
+    constructor(options) {
+        // 将 current 设置成响应式数据
+        Vue.util.defineReactive(this, 'current', '/');
+        // 存储 options
+        this.$options = options;
+        // 监听 url 的 hash 改变
+        window.addEventListener('hashchange', this.onhashchange.bind(this))
+        window.addEventListener('load', this.onhashchange.bind(this))
+    }
 
+    onhashchange(e) {
+        console.log('onhashchange', this.current)
+        this.current = e.target.location.hash.slice(1);
+    }
 }
+
 
 router.install = function (_vue) {
     // 保存 Vue 这个构造函数
@@ -16,6 +30,7 @@ router.install = function (_vue) {
         beforeCreate() {
             // 所有 Vue 组件都会走这个方法, 所以需要过滤一下
             if (this.$options.router) {
+                // 将创建好的 router 实例挂载在 Vue 原型上
                 Vue.prototype.$router = this.$options.router;
             }
         }
